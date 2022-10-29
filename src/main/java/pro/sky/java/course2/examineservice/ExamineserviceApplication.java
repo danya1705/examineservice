463de817bf84b999514385717dadce88e63f53b4
danya1705 <danya17@gmail.com>
2022-10-29 14:12:23 +0300

    first commit

---
+++ src/main/java/pro/sky/java/course2/examineservice/ExamineserviceApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamineserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamineserviceApplication.class, args);
+    }
+
+}
